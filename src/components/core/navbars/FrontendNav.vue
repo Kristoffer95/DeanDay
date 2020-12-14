4e3f91359d50e6330f7e78bd705e4e3aad938015
Kristoffer Gonzaga <kristoff.gonz@gmail.com>
2020-12-14 22:44:34 +0800

    'commit'

---
+++ src/components/core/navbars/FrontendNav.vue
@@ -0,0 +1,145 @@
+<template>
+  <!-- TOP Nav Bar -->
+<header id="main-header">
+   <div class="main-header">
+      <b-container fluid>
+         <b-row >
+            <b-col sm="12">
+               <nav class="navbar navbar-expand-lg navbar-light p-0">
+                  <a href="javascript:void(0)" class="navbar-toggler c-toggler collapsed" @click="openSidebar">
+                     <div class="navbar-toggler-icon" data-toggle="collapse">
+                        <span class="navbar-menu-icon navbar-menu-icon--top"></span>
+                        <span class="navbar-menu-icon navbar-menu-icon--middle"></span>
+                        <span class="navbar-menu-icon navbar-menu-icon--bottom"></span>
+                     </div>
+                  </a>
+                  <router-link class="navbar-brand" :to="homeURL"> <img class="img-fluid logo" :src="logo" alt="streamit" /></router-link>
+                  <b-collapse id="navbarSupportedContent" is-nav :visible="sidebar">
+                     <div class="menu-main-menu-container">
+                        <ul id="top-menu" class="navbar-nav ml-auto">
+                           <li v-for="(item,index) in items" :key="index" class="menu-item" :style="{'order': `${index > 1 ? index+1 : index}`}">
+                              <router-link :to="item.link">{{item.title}}</router-link>
+                           </li>
+                           <li class="menu-item nav-item nav-icon" v-nav-toggle style="order: 2">
+                              <a href="#" class="iq-user-dropdown search-toggle p-0 d-flex align-items-center" @mouseenter="showGamesModal = true" @mouseleave="showGamesModal = false">
+                                 Games
+                              </a>
+                              <transition name="fade">
+                                 <div class="iq-sub-dropdown iq-user-dropdown games-dropdown" v-show="showGamesModal" @mouseenter="showGamesModal = true" @mouseleave="showGamesModal = false">
+                                    <span >Bruce</span>
+                                    <span>Bruce Saves Christmas</span>
+                                    <!-- <div class="iq-card shadow-none m-0"> -->
+                                       <!-- <div class="iq-card-body p-0 pl-3 pr-3"> -->
+                                          <!-- <a :href="$router.resolve({name: 'dashboard.home-1'}).href" class="iq-sub-card setting-dropdown">
+                                             <div class="media align-items-center">
+                                             <div class="right-icon">
+                                                   <i class="ri-settings-4-line text-primary"></i>
+                                             </div>
+                                             </div>
+                                          </a> -->
+                                       <!-- </div> -->
+                                    <!-- </div> -->
+                                 </div>
+                              </transition>
+                           </li>
+                        </ul>
+                     </div>
+                  </b-collapse>
+                  <div class="mobile-more-menu">
+                     <b-navbar-toggle target="dropdownMenuButton" class="more-toggle">
+                        <i class="ri-more-line"></i>
+                     </b-navbar-toggle>
+                     <b-collapse id="dropdownMenuButton" class="more-menu">
+                        <div class="navbar-right position-relative">
+                           <slot name="responsiveRight"/>
+                        </div>
+                     </b-collapse>
+                  </div>
+                  <div class="navbar-right menu-right">
+                     <slot name="responsiveRight"/>
+                  </div>
+               </nav>
+               <div class="nav-overlay"></div>
+            </b-col>
+         </b-row>
+      </b-container>
+   </div>
+</header>
+  <!-- TOP Nav Bar END -->
+</template>
+<script>
+import { core, APPLOGONAME } from '../../../config/pluginInit'
+export default {
+   name: 'FrontendNav',
+   props: {
+      homeURL: { type: Object, default: () => ({ name: 'landing-page.landing-page' }) },
+      logo: { type: String, default: require('../../../assets/images/logo.png') },
+      items: { type: Array },
+      userprofile: { type: String }
+   },
+   data () {
+      return {
+         appName: APPLOGONAME,
+         sidebar: false,
+         showGamesModal: false
+      }
+   },
+   mounted () {
+      core.index()
+      document.addEventListener('click', this.closeSidebar, true)
+   },
+   destroyed () {
+      document.removeEventListener('click', this.closeSidebar, true)
+   },
+   methods: {
+   closeSidebar (e) {
+      if (!e.target.classList.contains('navbar-toggler-icon')) {
+         this.sidebar = false
+         document.getElementsByTagName('body')[0].classList.remove('nav-open')
+      }
+   },
+   openSidebar () {
+      document.getElementsByTagName('body')[0].classList.add('nav-open')
+      this.sidebar = true
+   }
+}
+}
+</script>
+
+<style lang="scss" scoped>
+   #top-menu {
+      display: flex;
+      justify-content: center;
+      // align-items: center;
+   }
+   .games-dropdown {
+      position: absolute;
+      right: 0;
+      top: 60px;
+      background-color: #191919;
+      // height: 100px;;
+      width: auto;
+      padding: 20px 20px 20px 50px;
+      display: flex;
+      justify-content: flex-start;
+      flex-direction: column;
+      // border: 1px solid red;
+      // transition: all 0.3s ease-in-out;
+      // max-width: 50px !important;
+      span {
+         white-space: nowrap;
+         text-align: start;
+         cursor: pointer;
+         padding: 2px 0;
+         &:hover {
+            color: red;
+         }
+      }
+   }
+   .fade-enter-active, .fade-leave-active {
+      transition: opacity 0.3s ease-in-out;
+   }
+   .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+      opacity: 0;
+   }
+</style>
---
+++ src/views/FrontendPages/ShopPage.vue
@@ -0,0 +1,185 @@
+<template>
+  <b-container fluid >
+    <!-- <b-row class="row"> -->
+    <div class="shop">
+      <div class="shop-title">
+        <span>Shop</span>
+      </div>
+    </div>
+    <b-row class="shop-page_row">
+      <b-col sm="12" class="overflow-hidden">
+        <div class="iq-main-header d-flex align-items-center justify-content-between">
+            <h4 class="main-title" v-b-tooltip.hover title="View All">
+            <router-link :to="{ name: 'landing-page.show-category' }" class="text-white">Suggested For You</router-link>
+            </h4>
+            <div class="d-flex slick-aerrow-block">
+                <button
+                  class="slick-prev slick-arrow"
+                  aria-label="Previous"
+                  type="button"
+                  style="display: block;"
+                  @click="prev()"
+                >
+                </button>
+                <button
+                  class="slick-next slick-arrow"
+                  aria-label="Next"
+                  type="button"
+                  style="display: block;"
+                  @click="next()"
+                >
+                </button>
+            </div>
+        </div>
+        <div class="suggestede-contens">
+            <Slick class="list-inline favorites-slider row p-0 mb-0" ref="dSlick" :option="suggestionSliderOption">
+              <li class="slide-item" v-for="(item,index) in suggestionData" :key="index">
+                  <router-link :to="{ name: 'landing-page.movie-detail' }">
+                    <div class="block-images position-relative">
+                        <div class="img-box">
+                          <!-- {{ item.images[0] }} -->
+                          <img :src="getImage(item.images[0])" class="img-fluid" alt="">
+                          <!-- <img src="@/assets/images/shop/Bruce/1.png" class="img-fluid" alt=""> -->
+                        </div>
+                        <div class="block-description">
+                          <h6>{{item.title}}</h6>
+                          <div class="movie-time d-flex align-items-center my-2">
+                              <div class="badge badge-secondary p-1 mr-2">{{item.age}}</div>
+                              <span class="text-white">{{item.time}}</span>
+                          </div>
+                          <div class="hover-buttons">
+                              <span class="btn btn-hover"><i class="fa fa-play mr-1" aria-hidden="true"></i>
+                              Buy now</span>
+                          </div>
+                        </div>
+                        <!-- <div class="block-social-info">
+                          <ul class="list-inline p-0 m-0 music-play-lists">
+                              <li><span><i class="ri-volume-mute-fill"></i></span></li>
+                              <li><span><i class="ri-heart-fill"></i></span></li>
+                              <li><span><i class="ri-add-line"></i></span></li>
+                          </ul>
+                        </div> -->
+                    </div>
+                  </router-link>
+              </li>
+            </Slick>
+        </div>
+      </b-col>
+    </b-row>
+    <!-- <div class="shop">
+      <div class="shop-title">
+        <span>Shop</span>
+      </div>
+
+    </div> -->
+    <!-- </b-row> -->
+  </b-container>
+</template>
+
+<script>
+// import Vue from 'vue'
+
+export default {
+  name: 'Shop',
+  data () {
+    return {
+      Products: [
+        {
+          name: 'B.R.U.C.E. Saves Christmas',
+          price: '£19.99',
+          images: ['@/assets/images/shop/1.png', '@/assets/images/shop/2.png', '@/assets/images/shop/3.png', '@/assets/images/shop/4.png', '@/assets/images/shop/5.png', '@/assets/images/shop/6.png', '@/assets/images/shop/7.png', '@/assets/images/shop/8.png', '@/assets/images/shop/9.png', '@/assets/images/shop/10.png']
+        }
+      ],
+      suggestionData: [
+        {
+          title: 'B.R.U.C.E. Saves Christmas',
+          age: '',
+          time: '£19.99',
+          images: ['bruce/1.png', 'bruce/2.png', 'bruce/3.png', 'bruce/4.png', 'bruce/5.png', 'bruce/6.png', 'bruce/7.png', 'bruce/8.png', 'bruce/9.png', 'bruce/10.png']
+        },
+        {
+          title: 'Sector X - The B.R.U.C.E. Project: Part 1',
+          age: '',
+          time: '£15.00',
+          images: ['bruce-project-part-1/1.png']
+        },
+        {
+          title: 'Sector X - The B.R.U.C.E. Project: Part 2',
+          age: '',
+          time: '£15.00',
+          images: ['bruce-project-part-2/1.png']
+        }
+        // {
+        //   title: 'Sector X - The B.R.U.C.E. Project: Part 2',
+        //   age: '',
+        //   time: '£15.00',
+        //   images: ['bruce-project-part-1/1.png']
+        // }
+        // { image: require('@/assets/images/frontend/suggested/01.jpg'), title: 'The Last Breath', age: '5+', time: '2h 30m' },
+        // { image: require('@/assets/images/frontend/suggested/02.jpg'), title: 'Last Night', age: '22+', time: '2h 30m' },
+        // { image: require('@/assets/images/frontend/suggested/03.jpg'), title: '1980', age: '25+', time: '2h 30m' },
+        // { image: require('@/assets/images/frontend/suggested/03.jpg'), title: '1980', age: '25+', time: '2h 30m' },
+        // { image: require('@/assets/images/frontend/suggested/03.jpg'), title: '1980', age: '25+', time: '2h 30m' }
+      ],
+      suggestionSliderOption: {
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        autoplay: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    getImage (path) {
+      return require(`@/assets/images/shop/${path}`)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .shop {
+    // border: 1px solid red;
+    margin-top: 80px;
+    // height: 100vh;
+    .shop-title {
+      text-align: center;
+      margin: 0 auto;
+      font-size: 34px;
+      font-weight: bold;
+    }
+  }
+  .shop-page_row {
+    // margin-top: 50px;
+    padding: 120px 0 100px 0;
+    // border: 1px solid red;
+  }
+</style>
